549f08972236faaab5592c62d5890dd630366e06
baiz mohamed hossam <baizmohamedhossam@gmail.com>
2023-04-17 10:09:00 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,73 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - gets length of string
+ * @str: string
+ * Return: length of str
+ */
+
+int _strlen(char *str)
+{
+	int l = 0;
+
+	while (*str++)
+		l++;
+	return (l);
+}
+
+/**
+ * _strcopy - copies string
+ * @src: source
+ * @dest: destination
+ * Return: dest
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] ; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: NULL if the function fails or pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = _strcopy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcopy(dog->owner, owner);
+	return (dog);
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# 0x0E. C - Structures, typedef
+![9ef84b3fd1636992602868e45d3062719326de0e](https://user-images.githubusercontent.com/125459327/232559105-bdc7215a-83ab-441a-9bb0-ff34696088e4.jpg)
