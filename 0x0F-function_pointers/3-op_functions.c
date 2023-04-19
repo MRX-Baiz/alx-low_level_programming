ec39287e770e979c211f571f0b70d21c83afdd04
baiz mohamed hossam <baizmohamedhossam@gmail.com>
2023-04-19 13:29:07 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-clac.h"
+
+/**
+ * op_add - adds 2 int
+ * @a: int
+ * @b: int
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts 2 int
+ * @a: int
+ * @b: int
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies 2 int
+ * @a: int
+ * @b: int
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides 2 int
+ * @a: int
+ * @b: int
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo 2 int
+ * @a: int
+ * @b: int
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
