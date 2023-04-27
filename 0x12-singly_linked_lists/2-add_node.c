fcee5643fc211e658e4600064f79edea15bcaf96
mohamed hossam <Baizmohamedhossam@gmail.com>
2023-04-27 08:23:11 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -29,7 +29,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!add)
 		return (NULL);
 	add->str = strdup(str);
-	add->len = l;
+	add->len = len;
 	add->next = *head;
 	*head = add;
 	return (*head);
